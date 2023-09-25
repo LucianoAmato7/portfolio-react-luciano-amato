03a62e0472c6be4bb3002e33acab7e12e72db462
LucianoAmato <luciano2697@outlook.com.ar>
2023-09-25 20:16:51 -0300

    firebase

--- src/components/profile/description/Description.jsx
+++ src/components/profile/description/Description.jsx
@@ -5,8 +5,7 @@ const Description = () => {
         <div className="mb-36 w-4/6 pt-24 flex flex-col">
             <h2 className="italic text-3xl mb-12 font-semibold">ACERCA DE MI:</h2>
             <p className="text-xl tracking-normal leading-relaxed">
-            Soy un apasionado recién graduado con un fuerte interés en el mundo de las tecnologías de la información. He adquirido sólidos 
-            conocimientos durante mis estudios y he demostrado habilidades técnicas a través de proyectos académicos y trabajos prácticos. Mi enfoque orientado a resultados, capacidad de resolución de problemas y habilidades de comunicación son activos que puedo aportar a un equipo de trabajo. Estoy comprometido con mi crecimiento profesional y ansío enfrentar nuevos desafíos en la industria IT.
+                Respetuoso, carismático y compañero. Profesional y ético en mi enfoque hacia el trabajo. Entusiasta por crecer en la industria IT, enfrento desafíos profesionales y técnicos. Experiencia en proyectos front y back-end, comprendiendo a fondo la arquitectura web. Listo para consolidar habilidades y crecer en el campo. Mi enfoque orientado a resultados, capacidad de resolución de problemas y habilidades de comunicación son activos que puedo aportar a un equipo de trabajo. Estoy comprometido con mi crecimiento profesional y ansío enfrentar nuevos desafíos.
             </p>
             <Technologies/>
         </div>    
--- src/components/ratingStars/RatingStars.jsx
+++ src/components/ratingStars/RatingStars.jsx
@@ -7,23 +7,18 @@ const RatingStars = () => {
   const [rating, setRating] = useState(0);
 
   const HandleButton = (e) => {
-
     e.preventDefault();
-    
+
     if (!rating == 0) {
-      
       const message = document.getElementById("ratingMessage").value;
       const RatingToSend = { value: rating, message: message };
-      console.log(RatingToSend)
+      console.log(RatingToSend);
       setRated(true);
       // ARCHIVAR CALIFICACIÓN
       // QUE SEA VIA MAIL Y A UNA DB COMO FIREBASE
-
-    }else {
-
+    } else {
       //COLOCAR UNA ALERTA ! EN DONDE SE DE AVISO QUE NO SE ELIGIO UN VALOR (ESTRELLITA)
       console.log("no se ha elegido un valor de estrellitas");
-
     }
   };
 
@@ -34,21 +29,15 @@ const RatingStars = () => {
     a11y: true,
     onChange: (newValue) => {
       setRating(newValue);
-    }
-  }
+    },
+  };
 
-  // const backGroundIMG = {
-  //   backgroundImage: 'url(img/backGroundimg-2.jpg)',
-  //   backgroundSize: 'cover', // Ajusta el tamaño de la imagen para cubrir completamente el fondo
-  //   backgroundPosition: 'center', // Centra la imagen
-  //   width: '100%', // Ajusta el ancho según sea necesario
-  //   height: '500px', // Ajusta la altura según sea necesario
-  // };
+
 
   return (
     <>
       {rated === true ? (
-        <Rated/>
+        <Rated />
       ) : (
         <div className="rounded-lg shadow-2xl text-center pt-1 mt-6 mb-20 w-1/3 bg-slate-50">
           <h1 className="text-3xl font-semibold my-6">
@@ -56,12 +45,12 @@ const RatingStars = () => {
           </h1>
           <div className="bg-slate-100 flex flex-col py-6 items-center rounded-b-lg">
             <h3 className="text-xl mb-2">¿Como puntuarías este Portfolio?</h3>
-            <ReactStars {...stars}/>
-            <textarea 
+            <ReactStars {...stars} />
+            <textarea
               className="rounded-lg m-6 p-2 h-20 w-3/4 resize-y"
               id="ratingMessage"
               placeholder="Deja un comentario, si quieres"
-            ></textarea >
+            ></textarea>
 
             <button
               className="rounded-lg bg-indigo-500 hover:bg-indigo-600 hover:scale-110 transition text-white max-w-fit px-4 py-2 text-xl"
